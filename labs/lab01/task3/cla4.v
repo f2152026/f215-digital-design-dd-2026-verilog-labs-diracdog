d9fa797108a25ef183d7271fce9e601c0fdbb8b7
Karan Makhija <f20241088@pilani.bits-pilani.ac.in>
2026-08-29 13:08:00 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -34,4 +34,28 @@ module cla4(
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
 
+  assign p0 = a[0] ^ b[0];
+  assign g0 = a[0] & b[0];
+  
+  assign p1 = a[1] ^ b[1];
+  assign g1 = a[1] & b[1];
+  
+  assign p2 = a[2] ^ b[2];
+  assign g2 = a[2] & b[2];
+  
+  assign p3 = a[3] ^ b[3];
+  assign g3 = a[3] & b[3];
+
+
+  assign c1 = g0 | (p0 & cin);
+  assign c2 = g1 | (p1 & g0) | (p1 & p0 & cin);
+  assign c3 = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & cin);
+  assign cout = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & cin);
+
+
+  assign sum[0] = p0 ^ cin;
+  assign sum[1] = p1 ^ c1;
+  assign sum[2] = p2 ^ c2;
+  assign sum[3] = p3 ^ c3;
+
 endmodule
